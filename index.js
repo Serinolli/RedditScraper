731ba4df74f62dc55ea0bd7323fcf595f5b176dd
Rafael Serinolli <rafael.serinolli2@gmail.com>
2023-11-21 17:11:23 -0300

    creating index.js and initating main function

---
+++ index.js
@@ -0,0 +1,14 @@
+const playwright = require("playwright");
+
+async function main() {
+    const broswer = await playwright.chromium.launch({
+        headless: false
+    })
+    const page = await (await broswer.newContext()).newPage();
+    await page.goto("https://old.reddit.com/r/programming/new/")
+    await broswer.close();
+}
+
+if(require.main === module) {
+    main();
+}
