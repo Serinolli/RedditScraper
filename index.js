4c2a7e17526b6ef349fd5b0a333c5d7f8d5bde1e
Rafael Serinolli <rafael.serinolli2@gmail.com>
2023-11-22 10:29:33 -0300

    adding logger in index.js and config npm start

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const playwright = require("playwright");
+const logger = require("./logger");
 
 async function getPagePosts(page) {
-    
+
 }
 
 async function main() {
+    logger.info("launching...");
     const broswer = await playwright.chromium.launch({
         headless: false
     })
