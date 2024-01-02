be97a0fc510dced4c4514bb1a0babd241262a3b5
Rafael Serinolli <rafael.serinolli2@gmail.com>
2024-01-02 12:06:11 -0300

    recovering url correctly

--- index.js
+++ index.js
@@ -1,6 +1,96 @@
 const playwright = require("playwright");
 const logger = require("./logger");
 
+async function parseComment(e) {
+    const things = await e.$$("> .sitetable > .thing");
+    let comments = [];
+    for (const thing of things) {
+      const attributes = await getAttributes(thing);
+  
+      let thingClass = attributes["class"];
+      let id = attributes["data-fullname"];
+      let children = await parseComment(await thing.$(".child"));
+  
+      let isDeleted = thingClass.includes("deleted");
+      let isCollapsed = thingClass.includes("collapsed");
+      let author = isDeleted ? "" : attributes["data-author"];
+      let time = await thing.$eval("time", (el) => el.getAttribute("datetime"));
+      let comment =
+        isDeleted || isCollapsed
+          ? ""
+          : await thing.$eval("div.md", (el) => el.innerText.trim());
+      let pointsText =
+        isDeleted || isCollapsed
+          ? ""
+          : await thing.$eval(
+              "span.score",
+              (el) => el.innerText.trim().split(" ")[0],
+            );
+  
+      let points = parseInt(pointsText);
+      points = isNaN(points) ? 0 : points;
+  
+      comments.push({
+        id,
+        author,
+        time,
+        comment,
+        points,
+        children,
+        isDeleted,
+        isCollapsed,
+      });
+    }
+  
+    return comments;
+  }
+
+async function getPostData({page,post}) {
+    logger.info("getting posts from actual page...", {post: post});
+
+    await page.goto(post.url);
+
+    const sitetable = await page.$('div.sitetable');
+    const thing = await sitetable.$('.thing');
+
+    let id = post.id;
+    let subreddit = post.subreddit;
+
+    const attributes = await getAttributes(thing);
+    let dataType = attributes["data-type"];
+    let dataURL = attributes["data-url"];
+    let isPromoted = attributes["data-promoted"] === "true";
+    let isGallery = attributes["data-gallery"] === "true";
+    let title = await page.$eval("a.title", (el) => el.innerText);
+    let points = parseInt(await sitetable.$(".score.unvoted").innerText);
+    let text = await sitetable.$("div.usertext-body").innerText;
+
+    let comments = [];
+    try {
+        comments = await parseComment(await page.$("div.commentarea"));
+    } catch (e) {
+        logger.error("error parsing comments", { error: e });
+    }
+
+    return {
+        id,
+        subreddit,
+        dataType,
+        dataURL,
+        isPromoted,
+        isGallery,
+        title,
+        timestamp: post.dt,
+        timestamp_millis: post.timestamp,
+        author: post.author,
+        url: post.url,
+        points: isNaN(points) ? 0 : points,
+        text,
+        comments,
+      };
+}
+
+
 async function getPagePosts(page) {
     logger.info("getting posts from actual page...");
     let posts = [];
@@ -16,9 +106,7 @@ async function getPagePosts(page) {
 
         const timeStamp = Date.parse(await time.getAttribute('datetime'));
         const author = await element.$eval('.author', (a) => a.innerText);
-        const url = await element.$eval('a.comments', (a) => {
-            a.getAttribute('href')
-        })
+        const url = await element.getAttribute("data-url");
 
         posts.push({id, subReddit, timeStamp, author, url})
     }
@@ -55,7 +143,14 @@ async function main() {
         let nextPageHREF = await page.$eval('.next-button a', (a) => a.href);
         await page.goto(nextPageHREF)
     }
-    logger.info(`found ${allPosts.length} posts`)
+    allPosts = allPosts.filter((post) => post.timeStamp > minDate);
+
+    let data = [];
+    logger.info("started getting posts informations...");
+    for (const post of allPosts) {
+        let postData = await getPostData({post,page});
+        data.push(postData);
+    }
     await broswer.close();
 }
 
