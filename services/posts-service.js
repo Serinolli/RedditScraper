8e81ff24e5b15e10b14e1baa7cd3759ac8fa1897
Rafael Serinolli <rafael.serinolli2@gmail.com>
2024-01-11 22:52:42 -0300

    removing unnecessary code

--- services/posts-service.js
+++ services/posts-service.js
@@ -3,8 +3,8 @@ const config = require("../config/request-settings.json");
 const logger = require("../logger")
 
 async function savePosts(posts) {
-  const options = { ...config, method: "POST", path: "/posts", data: posts };
-  const url = `${options.host}${options.port}/posts`;
+  const options = { ...config, method: "POST", path: "/posts" };
+  const url = `${options.host}${options.port}${options.path}`;
 
   await axios.post(url, posts)
   .then(response => {
